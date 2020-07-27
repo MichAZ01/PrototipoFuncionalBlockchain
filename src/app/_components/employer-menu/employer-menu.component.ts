f0d0ecc2c9f9647b6e6473e21228cc164f96de93
Sebastian Segura <sebassegurasoto@gmail.com>
2020-07-27 10:40:10 -0600

    Versión Final Paginas Empleador
    Se adjunta la versión final de las páginas del empleador con uso del menú.

--- src/app/_components/employer-menu/employer-menu.component.ts
+++ src/app/_components/employer-menu/employer-menu.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmployerComponent } from 'src/app/pages/employer/employer.component';
 @Component({
   selector: 'app-employer-menu',
   templateUrl: './employer-menu.component.html',
   styleUrls: ['./employer-menu.component.scss']
 })
 export class EmployerMenuComponent implements OnInit {
-
-  constructor() { }
+  flagHeader : number;
+  constructor(private employer: EmployerComponent) {
+    if(employer.flag){
+      this.flagHeader=0;
+    }
+    else{
+      this.flagHeader=1;
+    }
+   }
 
   ngOnInit(): void {
+  }
+
+  changeName(){
+    this.flagHeader=1;
+    this.employer.flag=false;
+  }
+
+  changeCompetencia(){
+    this.flagHeader=0;
+    this.employer.flag=true;
   }
 
 }
